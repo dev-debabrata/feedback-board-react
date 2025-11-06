a77f42d064b4fcf416293acf38b30a34d3aa1c09
Debabrata Das <debabratadas711@gmail.com>
2025-11-06 23:14:22 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { Header } from './components/Header';
+import { Opinions } from './components/Opinions';
+import { NewOpinion } from './components/NewOpinion';
+import { OpinionsContextProvider } from './store/opinions-context';
 
-const App = () => {
+function App() {
   return (
-    <div>App</div>
-  )
+    <>
+      <Header />
+      <main>
+        <OpinionsContextProvider>
+          <NewOpinion />
+          <Opinions />
+        </OpinionsContextProvider>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export function Header() {
+  return (
+    <header id="main-header">
+      <h1>Feedback Board</h1>
+      <p>
+        Strong feedbacks, judged by anonymous internet users. What could possibly
+        go wrong?
+      </p>
+    </header>
+  );
+}
